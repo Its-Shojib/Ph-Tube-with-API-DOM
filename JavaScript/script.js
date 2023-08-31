b32a6ff05917a9cc9eb390929531a1799db44978
Its_Shojib <mdshojib922@gmail.com>
2023-09-01 02:40:07 +0600

    Tab Added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,30 +21,22 @@ let tabLoad = async (categoryId) => {
     );
     let data = await response.json();
     let items = data.data;
-    console.log(items);
-    Sort(categoryId);
+    document.getElementById("sort-btn").addEventListener("click", function () {
+        setData(items.sort(byViews));
+        function byViews(a, b) {
+            return parseInt(b.others.views) - parseInt(a.others.views)
+        }
+    });
     setData(items)
+}
 
-}
-let Sort = async (id) => {
-        let res = await fetch(
-            `https://openapi.programming-hero.com/api/videos/category/${id}`
-        );
-        let data = await res.json();
-        let items2 = data.data;
-        document.getElementById("sort-btn").addEventListener("click", async function () {
-            setData(items2.sort(byID));
-            function byID(a, b) {
-                return parseInt(b.others.views) - parseInt(a.others.views)
-            }
-        });
-}
 // Set the data to container
 function setData(items) {
     let cardContainer = document.getElementById("card-container");
     let drawingContainer = document.getElementById("drawing-container");
     cardContainer.innerHTML = "";
     drawingContainer.innerHTML = "";
+    /* For Drawing Tab */
     if (items.length === 0) {
         let div = document.createElement("div");
         div.innerHTML = `
@@ -52,9 +44,9 @@ function setData(items) {
        <p class="text-3xl text-center ">Oops!! Sorry, There is no content here</p>
        `;
         drawingContainer.appendChild(div);
-
     }
     else {
+        /*for rest of tabs */
         items.forEach((item) => {
             let timeInSec = parseInt(item.others?.posted_date);
             let minutes = parseInt(timeInSec / 60);
@@ -62,20 +54,19 @@ function setData(items) {
             minutes = minutes % 60;
             let time = `${hour}hrs ${minutes} min ago`
 
-
             let div = document.createElement("div");
             div.innerHTML = `
-            <div class="card h-84 rounded-xl bg-purple-100 relative">
+            <div class="card h-80 rounded-xl bg-purple-100 relative">
                 <figure class="px-5 pt-5">
                     <img class="w-full h-40" src="${item.thumbnail}" alt="Ph-image" class="rounded-xl" />
                 </figure>
-                <span class="text-sm bg-black text-white absolute  bottom-[185px] right-8">${item.others?.posted_date ? `${time}` : ""}</span>
+                <span class="text-sm p-1 bg-black text-white absolute  bottom-[160px] right-8">${item.others?.posted_date ? `${time}` : ""}</span>
                 <div class="card-body flex flex-row justify-start gap-5">
                     <img src="${item.authors['0']?.profile_picture}" class="rounded-2xl w-10 h-10">
                     <div>
                         <h2 class="font-bold h-10">${item.title}</h2>
                         <span class="text-sm text-gray-400">${item.authors['0']?.profile_name} ${item.authors['0']?.verified ? "<img class='inline' src='./JavaScript/fi_10629607.svg'>" : ""}</span>
-                        <br><br>
+                        <br>
                         <p><span id="view-count">${item.others.views}</span> views</p>
                     </div>
                 </div>
--- JavaScript/test.js
+++
@@ -1,3 +0,0 @@
-var str = "5.5k";
-var num = parseFloat(str) * 1000;
-console.log(num);
