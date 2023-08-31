bf4387799ba5375f46c26614d74c4735604ca3ce
Its_Shojib <mdshojib922@gmail.com>
2023-08-31 23:14:49 +0600

    Time Added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -25,29 +25,37 @@ let tabLoad = async (categoryId) => {
     let drawingContainer = document.getElementById("drawing-container");
     cardContainer.innerHTML = "";
     drawingContainer.innerHTML = "";
-    if(items.length === 0){
-       let div = document.createElement("div");
-       div.innerHTML = `
+    if (items.length === 0) {
+        let div = document.createElement("div");
+        div.innerHTML = `
        <img class="text-center mx-auto" src="./Asset/Icon.png"> <br> <br>
        <p class="text-3xl text-center ">Oops!! Sorry, There is no content here</p>
        `;
-       drawingContainer.appendChild(div);
+        drawingContainer.appendChild(div);
 
     }
-    else{
+    // <span class="text-xs bg-black text-white">${hour} Hours ${minutes} Minutes Ago</span>
+    else {
         items.forEach((item) => {
+            let timeInSec = parseInt(item.others?.posted_date);
+            let minutes = parseInt(timeInSec / 60);
+            let hour = parseInt(minutes / 60);
+            minutes = minutes % 60;
+            let time = `${hour}hrs ${minutes} min ago`
+           
+
             let div = document.createElement("div");
             div.innerHTML = `
-            <div class="card h-84 rounded-xl bg-purple-100">
+            <div class="card h-84 rounded-xl bg-purple-100 relative">
                 <figure class="px-5 pt-5">
                     <img class="w-full h-40" src="${item.thumbnail}" alt="Ph-image" class="rounded-xl" />
                 </figure>
-                <p></p>
+                <span class="text-sm bg-black text-white absolute  bottom-[185px] right-8">${item.others?.posted_date ? `${time}`:""}</span>
                 <div class="card-body flex flex-row justify-start gap-5">
                     <img src="${item.authors['0']?.profile_picture}" class="rounded-2xl w-10 h-10">
                     <div>
                         <h2 class="font-bold h-10">${item.title}</h2>
-                        <span class="text-sm text-gray-400">${item.authors['0']?.profile_name} ${item.authors['0']?.verified?"<img class='inline' src='./JavaScript/fi_10629607.svg'>":""}</span>
+                        <span class="text-sm text-gray-400">${item.authors['0']?.profile_name} ${item.authors['0']?.verified ? "<img class='inline' src='./JavaScript/fi_10629607.svg'>" : ""}</span>
                         <br><br>
                         <p><span id="view-count">${item.others.views}</span> views</p>
                     </div>
