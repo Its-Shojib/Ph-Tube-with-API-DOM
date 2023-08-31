04f1f206ef83cdb2361a79b3487ae585f8cbbab0
Its_Shojib <mdshojib922@gmail.com>
2023-08-31 21:23:50 +0600

    Verified Added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,52 @@
-const handleCategory = async () => {
-    const response = await fetch(
-      "https://openapi.programming-hero.com/api/videos/categories"
+let handleCategory = async () => {
+    let response = await fetch(
+        "https://openapi.programming-hero.com/api/videos/categories"
     );
-    const data = await response.json();
+    let data = await response.json();
     let categories = data.data;
-      const tabContainer = document.getElementById("tab-container");
-      console.log(categories);
-      categories.forEach((category) => {
-      const div = document.createElement("div");
-      div.innerHTML = `
-          <a class="tab bg-gray-300 mx-3 my-1 rounded-lg text-black">${category.category}</a>
+    let tabContainer = document.getElementById("tab-container");
+    categories.forEach((category) => {
+        let div = document.createElement("div");
+        div.innerHTML = `
+          <a onclick="tabLoad('${category.category_id}')" class="tab bg-gray-300 mx-3 my-1 rounded-lg text-black">${category.category}</a>
           `;
-      tabContainer.appendChild(div);
+        tabContainer.appendChild(div);
     });
-  };
+};
+
+let tabLoad = async (categoryId) => {
+    console.log(categoryId);
+    let response = await fetch(
+        `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+    );
+    let data = await response.json();
+    console.log(data.data);
+    let items = data.data;
+
+    let cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+
+    items.forEach((item) => {
+        let div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card h-80 rounded-xl bg-slate-200">
+            <figure class="px-5 pt-5">
+                <img class="w-full h-40" src="${item.thumbnail}" alt="Ph-image" class="rounded-xl" />
+            </figure>
+            <p></p>
+            <div class="card-body flex flex-row justify-start gap-5">
+                <img src="${item.authors['0']?.profile_picture}" class="rounded-2xl w-10 h-10">
+                <div>
+                    <h2 class=" font-bold">${item.title}</h2>
+                    <span class="">${item.authors['0']?.profile_name} ${item.authors['0']?.verified?"<img class='inline' src='./JavaScript/fi_10629607.svg'>":""}</span>
+                    <br><br>
+                    <p><span id="view-count">${item.others.views}</span> views</p>
+                </div>
+            </div>
+        </div>`;
+        cardContainer.appendChild(div);
+    })
+}
+
 handleCategory();
+tabLoad("1000");
